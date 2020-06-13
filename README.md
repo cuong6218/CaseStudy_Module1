b00adb054b16b176138a321272c07db90d744e64
Rea Sakyo <cuong6218@gmail.com>
2020-06-13 17:13:31 +0700

    sua file README và them file Book.js

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CaseStudy_Module1
+1. Tạo đối tượng Book và Book Store
+2. Tạo giao diện hiển thị danh sách thông tin sách và giao diện thêm thông tin sách
+3. Xử lý logic chức năng hiển thị danh sách thông tin sách và chức năng thêm thông tin sách
+4. Tạo giao diện chức năng xóa thông tin sách
+5. Xử lý logic chức năng xóa thông tin sách
+6. Tạo giao diện chức năng sửa thông tin sách
+7. Xử lý logic chức năng sửa thông tin sách
---
+++ js/Book.js
@@ -0,0 +1,72 @@
+class Book{
+    constructor(name, kind, author, publish, price, desc){
+        this._name = name;
+        this._kind = kind;
+        this._author = author;
+        this._publish = publish;
+        this._price = price;
+        this._desc = desc;
+        this._img = "";
+    }
+    get name(){
+        return this._name;
+    }
+    set name(value){
+        this._name = value;
+    }
+    get kind(){
+        return this._kind;
+    }
+    set kind(value){
+        this._kind = value;
+    }
+    get author(){
+        return this._author;
+    }
+    set author(value){
+        this._author = value;
+    }
+    get publish(){
+        return this._publish;
+    }
+    set publish(value){
+        this._publish = value;
+    }
+    get price(){
+        return this._price;
+    }
+    set price(value){
+        this._price = value;
+    }
+    get desc(){
+        return this._desc;
+    }
+    set desc(value){
+        this._desc = value;
+    }
+    get img(){
+        return this._img;
+    }
+    set img(value){
+        this._img = value;
+    }
+    edit(name, kind, author, publish, price, desc, img){
+        this.name = name;
+        this.kind = kind;
+        this.author = author;
+        this.publish = publish;
+        this.price = price;
+        this.desc = desc;
+    }
+    getHtml(){
+        let table = `<tr>
+                        <td>${this._name}</td>
+                        <td>${this._kind}</td>
+                        <td>${this._author}</td>
+                        <td>${this._publish}</td>
+                        <td>${this._price}</td>
+                        <td>${this._desc}</td>
+                        <td>${this._img}</td>
+                    </tr>`
+    }
+}
