9fc9dde1df456e6d65c9165c56ef6c84673f76b1
Rea Sakyo <cuong6218@gmail.com>
2020-06-13 17:31:20 +0700

    them doi tuong BookStore

--- js/BookStore.js
+++ js/BookStore.js
@@ -1,8 +1,23 @@
-class BookStore{
-    constructor(name){
+class BookStore {
+    constructor(name) {
         this.name = name;
         this.books = [];
         this.data = [];
     }
-    
+    init() {
+        for (let i = 0; i < this.data.length; i++) {
+            let book = new Book(this.data[i]._name, this.data[i]._kind, this.data[i]._author, this.data[i]._publish, this.data[i]._price, this.data[i]._desc);
+            book.img = this.data[i]._img;
+            this.addBook(book);
+        }
+    }
+    addBook(book) {
+        this.books.push(book);
+    }
+    getHtml() {
+        let table = `<tr>
+                        <td>${this.books.getHtml()}</td>
+                    </tr>`
+        return table;
+    }
 }
